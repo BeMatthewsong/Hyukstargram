26c9a66afece097d12e0a700cb43dcace743ef7c
Matthew Song <songww1997@naver.com>
2024-04-15 14:16:06 +0900

    feat: 라우팅 설정 추가

---
+++ src/components/Router.tsx
@@ -0,0 +1,32 @@
+import Home from "@/pages/home";
+import Post from "@/pages/posts";
+import PostDetail from "@/pages/posts/detail";
+import PostEdit from "@/pages/posts/edit";
+import PostNew from "@/pages/posts/new";
+import Profile from "@/pages/profile";
+import ProfileEdit from "@/pages/profile/edit";
+import Search from "@/pages/search";
+import Login from "@/pages/user/login";
+import Signup from "@/pages/user/signup";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/posts" element={<Post />} />
+      <Route path="/posts/:id" element={<PostDetail />} />
+      <Route path="/posts/new" element={<PostNew />} />
+      <Route path="/posts/edit/:id" element={<PostEdit />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/profile/edit" element={<ProfileEdit />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/notification" element={<Search />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/*" element={<Navigate replace to="/" />} />
+    </Routes>
+  );
+};
+
+export default Router;
