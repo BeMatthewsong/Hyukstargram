f44d7d34c90c1d212584d9ead8bdd99b39db76a5
Matthew Song <songww1997@naver.com>
2024-04-17 12:15:45 +0900

    env: 파이어베이스 세팅

---
+++ firebaseApp.ts
@@ -0,0 +1,28 @@
+import { initializeApp, FirebaseApp, getApp } from "firebase/app";
+import "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+export let app: FirebaseApp;
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_ID,
+};
+
+try {
+  app = getApp("app");
+} catch (e) {
+  app = initializeApp(firebaseConfig, "app");
+}
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
+export default firebase;
