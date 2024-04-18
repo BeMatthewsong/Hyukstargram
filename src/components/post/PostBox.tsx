036ec2587017d2f4789f88b0b6788d79850f93b7
Matthew Song <songww1997@naver.com>
2024-04-18 17:39:11 +0900

    feat: firestore db 연동

--- src/components/post/PostBox.tsx
+++ src/components/post/PostBox.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { AiFillHeart } from "react-icons/ai";
 import { FaCommentAlt, FaUserCircle } from "react-icons/fa";
 import { PostProps } from "@/types/postType";
+import { useContext } from "react";
+import AuthContext from "@contexts/AuthContext";
 
 interface PostBoxProps {
   posts: PostProps[];
@@ -9,49 +11,64 @@ interface PostBoxProps {
 }
 
 const PostBox = ({ posts, onDelete }: PostBoxProps) => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="post">
-      {posts?.map((post) => (
-        <div className="post__box" key={post?.id}>
-          <Link to={`/posts/${post?.id}`}>
-            <div className="post__box-profile">
-              <div className="post__flex">
-                {post?.profileUrl ? (
-                  <img
-                    src={post?.profileUrl}
-                    alt="profile"
-                    className="post__box-profile-icon"
-                  />
-                ) : (
-                  <FaUserCircle className="post__box-profile-icon" />
-                )}
-                <div className="post__email">{post?.email}</div>
-                <div className="post__createdAt">{post?.createdAt}</div>
+      {posts?.length > 0 ? (
+        posts?.map((post) => (
+          <div className="post__box" key={post?.id}>
+            <Link to={`/posts/${post?.id}`}>
+              <div className="post__box-profile">
+                <div className="post__flex">
+                  {post?.profileUrl ? (
+                    <img
+                      src={post?.profileUrl}
+                      alt="profile"
+                      className="post__box-profile-icon"
+                    />
+                  ) : (
+                    <FaUserCircle className="post__box-profile-icon" />
+                  )}
+                  <div className="post__email">{post?.email}</div>
+                  <div className="post__createdAt">{post?.createdAt}</div>
+                </div>
+                <div className="post__box-content">{post?.content}</div>
               </div>
-              <div className="post__box-content">{post?.content}</div>
-            </div>
-          </Link>
-          <div className="post__box-footer">
-            {/* 게시물 주인일 때 */}
-            <div className="post__button-box">
-              <button type="button" className="post__likes">
-                <AiFillHeart /> {post?.likeCount || 0}
-              </button>
-              <button type="button" className="post__comments">
-                <FaCommentAlt /> {post?.comments?.length || 0}
-              </button>
-            </div>
-            <div className="post__button-box">
-              <button type="button" className="post__edit">
-                <Link to={`/posts/edit/${post?.id}`}>수정</Link>
-              </button>
-              <button type="button" className="post__delete" onClick={onDelete}>
-                삭제
-              </button>
+            </Link>
+            <div className="post__box-footer">
+              {user?.uid === post?.uid && (
+                <>
+                  <div className="post__button-box">
+                    <button type="button" className="post__likes">
+                      <AiFillHeart /> {post?.likeCount || 0}
+                    </button>
+                    <button type="button" className="post__comments">
+                      <FaCommentAlt /> {post?.comments?.length || 0}
+                    </button>
+                  </div>
+                  <div className="post__button-box">
+                    <button type="button" className="post__edit">
+                      <Link to={`/posts/edit/${post?.id}`}>수정</Link>
+                    </button>
+                    <button
+                      type="button"
+                      className="post__delete"
+                      onClick={onDelete}
+                    >
+                      삭제
+                    </button>
+                  </div>
+                </>
+              )}
             </div>
           </div>
+        ))
+      ) : (
+        <div className="post__no-posts">
+          <div className="post__text">게시글이 없습니다.</div>
         </div>
-      ))}
+      )}
     </div>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,34 @@
-import { posts } from "@/constatnts/postMockData";
+import { db } from "@/firebaseApp";
+import { PostProps } from "@/types/postType";
 import PostBox from "@components/post/PostBox";
 import PostForm from "@components/post/PostForm";
 import PostTab from "@components/post/PostTab";
+import AuthContext from "@contexts/AuthContext";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
 
 const Home = () => {
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const { user } = useContext(AuthContext);
+
   // TODO: 함수 내용 추가
   const handleFileUpload = () => {};
   const handleDelete = () => {};
+
+  useEffect(() => {
+    if (user) {
+      let postRef = collection(db, "posts");
+      let postQuery = query(postRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(postQuery, (snapShot) => {
+        let dataObj = snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc?.id,
+        }));
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, []);
 
   return (
     <main className="home">
