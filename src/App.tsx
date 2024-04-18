fb8729c8d79287662e5e46624f4aa7cf62b78025
Matthew Song <songww1997@naver.com>
2024-04-18 14:14:01 +0900

    feat: 회원가입 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import Layout from "@components/Layout";
 import Router from "@components/Router";
-import { getAuth } from "firebase/auth";
-import { useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 import { app } from "./firebaseApp";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "@components/loader/Loader";
 
 function App() {
   const auth = getAuth(app);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    // 로그인 상태를 살펴보는 옵저버
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
 
   return (
     <Layout>
-      <Router isAuthenticated={isAuthenticated} />
+      <ToastContainer />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </Layout>
   );
 }
